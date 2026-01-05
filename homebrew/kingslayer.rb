ae9437bd3596ad5684d4f7526f309f7bc467530d
Michael Gilhooly <michael.gilhooly@gmail.com>
2026-01-05 23:28:01 +1100

    Update Homebrew formula with v0.1.0 SHA256 hashes
    SHA256 hashes for v0.1.0 release:
    - macOS ARM64: e71a0b6ee9f588f482c03448d31a409d2ea26212c0e08d5f89972299b0660e41
    - macOS x86_64: 6414de3e3002c7f2f1a7157b87ebb91dda6484bf6586b060491e781b3a924ed5
    - Linux x86_64: e15aa0a18d44fd28aa68a6081cb4452f4662ca65922b3b7bd8305c53ad169144

--- homebrew/kingslayer.rb
+++ homebrew/kingslayer.rb
@@ -7,16 +7,16 @@ class Kingslayer < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/str4nge-m4g1c/regicide-tui/releases/download/v#{version}/kingslayer-macos-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_ARM64_MACOS" # Run: shasum -a 256 kingslayer-macos-aarch64.tar.gz
+      sha256 "e71a0b6ee9f588f482c03448d31a409d2ea26212c0e08d5f89972299b0660e41"
     else
       url "https://github.com/str4nge-m4g1c/regicide-tui/releases/download/v#{version}/kingslayer-macos-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_X86_64_MACOS" # Run: shasum -a 256 kingslayer-macos-x86_64.tar.gz
+      sha256 "6414de3e3002c7f2f1a7157b87ebb91dda6484bf6586b060491e781b3a924ed5"
     end
   end
 
   on_linux do
     url "https://github.com/str4nge-m4g1c/regicide-tui/releases/download/v#{version}/kingslayer-linux-x86_64.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_LINUX" # Run: shasum -a 256 kingslayer-linux-x86_64.tar.gz
+    sha256 "e15aa0a18d44fd28aa68a6081cb4452f4662ca65922b3b7bd8305c53ad169144"
   end
 
   def install
